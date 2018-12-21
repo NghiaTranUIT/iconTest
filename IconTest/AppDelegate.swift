c3185cd41fa1247311119f99c5f0fe96a28d8ca1
IndrekV <indzuliin@gmail.com>
2018-12-21 10:35:33 +0200

    Initial Commit

---
+++ IconTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  IconTest
+//
+//  Created by Mac mini on 21/12/2018.
+//  Copyright © 2018 Mac mini. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ IconTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IconTest
+//
+//  Created by Mac mini on 21/12/2018.
+//  Copyright © 2018 Mac mini. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
